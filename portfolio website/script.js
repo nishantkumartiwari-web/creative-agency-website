fc0530ecf65613da8b2242c7f50d5fdbd6a9a56d
Nishant tiwari <nishantkumartiwari@caplineservices.com>
2026-09-11 17:48:47 +0530

    added timer in msg for error and success

--- portfolio website/script.js
+++ portfolio website/script.js
@@ -1,3 +1,4 @@
+
 const form = document.getElementById("contactForm");
 
 form.addEventListener("submit", function (event) {
@@ -7,21 +8,52 @@ form.addEventListener("submit", function (event) {
   const name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   const message = document.getElementById("message").value.trim();
+
   const error = document.getElementById("error");
+  const success = document.getElementById("success");
 
   if (name === "" || email === "" || message === "") {
+
     error.textContent = "Please fill all fields.";
+    error.style.display = "block";
+
+    setTimeout(() => {
+
+      // document.getElementById("success").style.display = "none";
+      error.style.display = "none";
+
+    }, 2000);
+
     return;
   }
 
   if (!email.includes("@")) {
+
     error.textContent = "Please enter a valid email.";
+    error.style.display = "block";
+
+    setTimeout(() => {
+
+      // document.getElementById("success").style.display = "none";
+      error.style.display = "none";
+
+    }, 2000);
+
     return;
   }
 
   // error.textContent = "Message sent successfully!";
-  const success = document.getElementById("success");
+
   success.textContent = "Message sent successfully!";
+  success.style.display = "block";
+
+  setTimeout(() => {
+
+    success.style.display = "none";
+
+    // document.getElementById("error").style.display = "none";
+
+  }, 2000);
 
   form.reset();
 });
